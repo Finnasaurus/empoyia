bb1b5897f4ddc1a17fe2c230f0da0b57efc05f5c
ChenQirui1 <ivanchen.code@gmail.com>
2025-02-19 18:16:11 +0800

    [sketch] added input and distance

---
+++ myopia.js
@@ -0,0 +1,20 @@
+function pixelsToDistance(pixels) {
+  return 0.0002645833 * pixels;
+}
+
+function degreesDistToBlur(degrees, objectDistance) {
+  //convert degrees to diopters
+  //e.g. 300 degrees = 3 diopters
+  let diopters = degrees / 100;
+
+  //calulate far point
+  let farPoint = 1 / diopters;
+
+  //calculate blur
+  //assuming pupil diametter of 4mm
+  let pupilDiameter = 4;
+
+  let blur = (pupilDiameter / 2) * abs(1 / farPoint - 1 / objectDistance);
+
+  return blur;
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,64 @@
+let img;
+let input;
+let blurAmount = 0;
+let onSubmit = false;
+
+// Load the image.
+function preload() {
+  img = loadImage("/assets/snellen-chart-crop.jpg");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(displayWidth, displayHeight);
+  background(100);
+  frameRate(30);
+
+  // Create an input and place it beneath the canvas.
+  input = createInput("");
+  input.position(0, 100);
+
+  // Call repaint() when input is detected.
+  input.input(repaint);
+
+  describe(
+    "A gray square with a text input bar beneath it. Any text written in the input appears in the middle of the square."
+  );
 }
 
 function draw() {
-  background(220);
+  if (onSubmit) {
+    drawImage();
+  }
+}
+
+function drawImage() {
+  if (blurAmount < 0) {
+    blurAmount = 0;
+  }
+  image(img, 0, 0, width, height);
+  filter(BLUR, blurAmount);
+}
+
+// Paint the background gray and display the input's value.
+function repaint() {
+  background(200);
+  let msg = input.value();
+  console.log(msg);
+}
+
+function keyPressed() {
+  if (key === "j") {
+    blurAmount -= 1;
+    console.log(blurAmount);
+  }
+  if (key === "k") {
+    blurAmount += 1;
+    console.log(blurAmount);
+  }
+  if (keyCode === 13) {
+    console.log(input.value());
+    console.log("Enter pressed");
+    input.hide();
+    onSubmit = true;
+  }
 }
